421a50a2be541b7d9c235a223a3a3a74072aee14
Christopher Bonner <cbonner.dev@outlook.com>
2024-01-17 00:16:59 +0000

    Got plaid to exchange public token

--- amplify/backend/api/cachebackend/src/app/routes/plaid_client.py
+++ amplify/backend/api/cachebackend/src/app/routes/plaid_client.py
@@ -1,10 +1,12 @@
 import json, time, plaid
 
-from flask import Blueprint, jsonify
+from flask import Flask, request, Blueprint, jsonify
 
 from plaid.model.link_token_create_request import LinkTokenCreateRequest
 from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
 from plaid.model.country_code import CountryCode
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+
 
 from ..models import db, User, Settings
 from ..plaid_config import client, products, PLAID_REDIRECT_URI, PLAID_COUNTRY_CODES
@@ -30,3 +32,19 @@ def create_link_token():
         return jsonify(response.to_dict())
     except plaid.ApiException as e:
         return json.loads(e.body)
+    
+@plaid_routes.route('/api/plaid/exchange_public_token', methods=['POST'])
+def exchange_public_token():
+    public_token = request.form['public_token']
+
+    new_request = ItemPublicTokenExchangeRequest(public_token=public_token)
+    response = client.item_public_token_exchange(new_request)
+
+    # These values should be saved to a persistent database and
+
+    # associated with the currently signed-in user
+
+    access_token = response['access_token']
+    item_id = response['item_id']
+
+    return jsonify({'public_token_exchange': 'complete'})
