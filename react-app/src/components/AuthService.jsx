dd51a7dfb73260eea36b055bb799d668cb4c86de
Christopher Bonner <cbonner.dev@outlook.com>
2024-06-21 18:40:30 +0100

    Changing local port number since in use

--- react-app/src/components/AuthService.jsx
+++ react-app/src/components/AuthService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import httpClient from "../httpClient";
 
-const API_URL = "http://localhost:8000/api/auth/";
+const API_URL = "http://localhost:8080/api/auth/";
 
 class AuthService {
   
