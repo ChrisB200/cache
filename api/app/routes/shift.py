fcaeb4e2a2287810b4b8a9bd9d4432e74e46fd0e
Christopher Bonner <cbonner.dev@outlook.com>
2024-08-24 01:51:54 +0100

    method to return all payslips

--- api/app/routes/shift.py
+++ api/app/routes/shift.py
@@ -1,11 +1,15 @@
 
 from flask import Blueprint, request, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import db, User, Shift
 
 shift = Blueprint("shift", __name__)
 
 
-@shift.route("/api/shift/login", methods=["POST"])
+@shift.route("/api/shift/all", methods=["POST"])
+@login_required
 def placeholder():
-    pass
+    if not current_user:
+        return jsonify({"error": "No user logged in"}), 401
+    
+    return jsonify([payslip.to_json() for payslip in current_user.payslips])
