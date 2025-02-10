a19ce4687d1b031e29fbc30d36dd83a20d83e117
Christopher Bonner <cbonner.dev@outlook.com>
2025-02-10 12:38:01 +0000

    rebuild containers

--- build/build.sh
+++ build/build.sh
@@ -6,4 +6,5 @@ docker stop react-app || true
 docker rm react-app || true
 docker stop flask-api || true
 docker rm flask-api || true
+docker compose build --no-cache
 docker compose up -d --force-recreate
