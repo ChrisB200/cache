b5855cd5da83f401cee3808ac95b87913e5def3e
Christopher Bonner <cbonner.dev@outlook.com>
2025-02-10 12:36:54 +0000

    fixed typo in build script

--- build/build.sh
+++ build/build.sh
@@ -5,5 +5,5 @@ python build/symlink.py --name "$1"
 docker stop react-app || true
 docker rm react-app || true
 docker stop flask-api || true
-docker rm flaks-api || true
+docker rm flask-api || true
 docker compose up -d --force-recreate
